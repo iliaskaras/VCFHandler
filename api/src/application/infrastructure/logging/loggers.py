25ea1d40546bb1c988739f94cb1ba12446684c09
Ilias Karatsin <hlias.karas@yahoo.gr>
2021-05-15 22:55:34 +0300

    Add custom logger

---
+++ api/src/application/infrastructure/logging/loggers.py
@@ -0,0 +1,28 @@
+import logging
+import sys
+from logging import Logger
+
+
+def configure_application_logger(name: str) -> Logger:
+    """
+    Configuration of the application stderr channel logs.
+
+    @:param name: Logger name.
+
+    @:return: The application Logger.
+    """
+
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.INFO)
+
+    stream_handler = logging.StreamHandler(sys.stderr)
+    stream_handler.setFormatter(
+        logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(funcName)s")
+    )
+
+    logger.addHandler(stream_handler)
+
+    return logger
+
+
+LOGGER = configure_application_logger("VCF LOGGER")
