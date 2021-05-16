2ec35f5f026fe691a251110715d8947e52dc56bc
Ilias Karatsin <hlias.karas@yahoo.gr>
2021-05-16 23:04:29 +0300

    Append error code to the http error handler

--- api/src/application/rest_api/configurations.py
+++ api/src/application/rest_api/configurations.py
@@ -1,4 +1,4 @@
-from typing import Dict, Union, List
+from typing import Dict, Union, List, Tuple
 
 from flask_restplus import Api
 
@@ -8,10 +8,10 @@ from application.rest_api.errors import PublicHttpError
 
 def configure_api_error_handling(api: Api) -> None:
     @api.errorhandler(PublicHttpError)
-    def http_error_handler(error: PublicHttpError) -> Dict[str, Union[List[Dict[str, str]], int]]:
+    def http_error_handler(error: PublicHttpError) -> Tuple[Dict[str, Union[List[Dict[str, str]], int]], int]:
         LOGGER.exception(error)
 
         return {
             'errors': [_error.dump() for _error in error.errors],
             'errorCode': error.ERROR_CODE
-        }
+        }, error.ERROR_CODE
