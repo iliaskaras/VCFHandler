7137924c3915b61e67a8620c0da0f72dfa39c0ac
Ilias Karatsin <hlias.karas@yahoo.gr>
2021-05-17 03:34:28 +0300

    Add VCF File post request.

--- api/src/application/vcf_files/errors.py
+++ api/src/application/vcf_files/errors.py
@@ -4,3 +4,8 @@ from application.infrastructure.error.errors import InvalidArgumentError
 class VcfRowsByIdNotExistError(InvalidArgumentError):
     message = "Vcf Rows By Id Not Exist Error."
     error_type = "VcfRowsByIdNotExistError"
+
+
+class VcfDataAppendError(InvalidArgumentError):
+    message = "Vcf Data Append Error."
+    error_type = "VcfDataAppendError"
--- api/src/application/vcf_files/factories.py
+++ api/src/application/vcf_files/factories.py
@@ -1,8 +1,14 @@
-from application.rest_api.vcf_files.operations import FilterVcfFile
-from application.vcf_files.services import VcfFilePaginationService
+from application.rest_api.vcf_files.operations import FilterVcfFile, AppendToVcfFile
+from application.vcf_files.services import VcfFilePaginationService, AppendDataToVcfFileService
 
 
 def vcf_file_pagination_service() -> VcfFilePaginationService:
     return VcfFilePaginationService(
         filter_vcf_file=FilterVcfFile(),
+    )
+
+
+def append_data_to_vcf_file_service() -> AppendDataToVcfFileService:
+    return AppendDataToVcfFileService(
+        append_to_vcf_file=AppendToVcfFile(),
     )
